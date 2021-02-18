cbc0c9fb945c77f8a2788986043e8f102199f6a8
Krzem <krzem5.dev@gmail.com>
2021-02-18 13:06:21 +0100

    Push Update 02/18/2021, 13:06:14

--- src/main.py
+++ src/main.py
@@ -1,200 +1,242 @@
-import copy
+import random
 
 
 
-def dominos(d):
-    ld=[]
-    for n in range(d+1):
-        for n2 in range(n+1):
-            ld+=[[str(n2),str(n)]]
-    return ld
-def print_format(g):
-    s='+-'*len(g[0])+'+\n'
-    for i in range(len(g)):
-        s2='+'
-        for j in range(len(g[0])):
-            c=g[i][j]
-            if j==0:s+='|'
-            else:s+=c[1][1]
-            if i<len(g)-1:s2+=c[1][0]+'+'
-            else:s2+='-+'
-            s+=c[0]
-        s+='|\n'
-        s+=s2+'\n'
-    return s[:-1]
-def index(g,x,y):return -1<x<len(g[0])and -1<y<len(g)
-def occ(g,d):
-    occ=[]
-    chk=[]
-    for y in range(len(g)):
-        for x in range(len(g[0])):
-            if [x,y] not in chk and g[y][x][0]==d[0]:
-                if index(g,x,y-1)and g[y-1][x][0]==d[1]and g[y-1][x][1][0]==' ':occ.append([[x,y],[x,y-1],d]);chk+=[[x,y],[x,y-1]]
-                if index(g,x+1,y)and g[y][x+1][0]==d[1]and g[y][x+1][1][1]==' ':occ.append([[x,y],[x+1,y],d]);chk+=[[x,y],[x+1,y]]
-                if index(g,x,y+1)and g[y+1][x][0]==d[1]and g[y][x][1][0]==' ':occ.append([[x,y],[x,y+1],d]);chk+=[[x,y],[x,y+1]]
-                if index(g,x-1,y)and g[y][x-1][0]==d[1]and g[y][x][1][1]==' ':occ.append([[x,y],[x-1,y],d]);chk+=[[x,y],[x-1,y]]
-    return occ
-def remove(g,d,dom,d_doms):
-    dom.remove(d[2])
-    d_doms+=d[:2]
-    a1,a2,b1,b2=d[0][0],d[0][1],d[1][0],d[1][1]
-    x,y=b1-a1,b2-a2
-    if x==1:
-        g[a2][a1][1]=['-','|']
-        g[b2][b1][1][0]='-'
-        if index(g,a1,a2-1):g[a2-1][a1][1][0]='-'
-        if index(g,b1,b2-1):g[b2-1][b1][1][0]='-'
-        if index(g,b1+1,b2):g[b2][b1+1][1][1]='|'
-    if x==-1:
-        g[b2][b1][1]=['-','|']
-        g[a2][a1][1][0]='-'
-        if index(g,b1,b2-1):g[b2-1][b1][1][0]='-'
-        if index(g,a1,a2-1):g[a2-1][a1][1][0]='-'
-        if index(g,a1+1,a2):g[a2][a1+1][1][1]='|'
-    if y==1:
-        g[a2][a1][1][1]='|'
-        g[b2][b1][1]=['-','|']
-        if index(g,b1+1,b2):g[b2][b1+1][1][1]='|'
-        if index(g,a1+1,a2):g[a2][a1+1][1][1]='|'
-        if index(g,a1,a2-1):g[a2-1][a1][1][0]='-'
-    if y==-1:
-        g[b2][b1][1][1]='|'
-        g[a2][a1][1]=['-','|']
-        if index(g,a1+1,a2):g[a2][a1+1][1][1]='|'
-        if index(g,b1+1,b2):g[b2][b1+1][1][1]='|'
-        if index(g,b1,b2-1):g[b2-1][b1][1][0]='-'
-    return g,dom,d_doms
-def wall_cnt(g,x,y):
-    w=0
-    w_=[]
-    if (index(g,x,y-1)and g[y-1][x][1][0]=='-')or not index(g,x,y-1):w+=1
-    elif index(g,x,y-1):w_+=[[x,y-1,0]]
-    if (index(g,x+1,y)and g[y][x+1][1][1]=='|')or not index(g,x+1,y):w+=1
-    elif index(g,x+1,y):w_+=[[x+1,y,1]]
-    if g[y][x][1][0]=='-':w+=1
-    elif index(g,x,y+1):w_+=[[x,y+1,2]]
-    if g[y][x][1][1]=='|':w+=1
-    elif index(g,x-1,y):w_+=[[x-1,y,3]]
-    return [w,w_]
-def solve(grid,dom):
-    W,H=len(grid[0]),len(grid)
-    d_doms=[]
-    if type(grid[0][0])!=list:
-        g=[]
-        [[g.append([]),[g[i].append([grid[i][j],[' ',' ']])for j in range(W)]] for i in range(H)]
-    else:
-        g=grid
-        del grid
-    while True:
-        done=True
-        #M1
-        rm_l=[]
-        [rm_l.append(occ(g,d)[0]) for d in dom if len(occ(g,d))==1]
-        for r in rm_l:g,dom,d_doms=remove(g,r,dom,d_doms);done=False
-        #M2
-        for y in range(H):
-            for x in range(W):
-                if [x,y] not in d_doms:
-                    w=wall_cnt(g,x,y)[1]
-                    for d in w:
-                        if [g[y][x][0],g[d[1]][d[0]][0]] not in dom and [g[d[1]][d[0]][0],g[y][x][0]] not in dom:
-                            if d[2]==0 and index(g,x,y-1):g[y-1][x][1][0]='-'
-                            elif d[2]==1 and index(g,x+1,y):g[y][x+1][1][1]='|'
-                            elif d[2]==2:g[y][x][1][0]='-'
-                            elif d[2]==3:g[y][x][1][1]='|'
-        #M3
-        """for y in range(H):
-            for x in range(W):
-                if [x,y] not in d_doms:
-                    if 4-wall_cnt(g,x,y)[0]>1:
-                        w=wall_cnt(g,x,y)[1]
-                        exit_=False
-                        for opt in w:
-                            if not exit_:
-                                g_,dom_,d_doms_=copy.deepcopy(g),copy.deepcopy(dom),copy.deepcopy(d_doms)
-                                d=[g[y][x][0],g[opt[1]][opt[0]][0]]
-                                if d not in dom:d.reverse()
-                                if d in dom:
-                                    s=solve(*remove(g_,[[x,y],opt[:2],d],dom_,d_doms_)[:2])
-                                    if len(s[0])==0:exit_=True
-                                    if exit_:g,dom,d_doms=remove(g,[[x,y],opt[:2],d],dom,d_doms);done=False
-                                else:
-                                    if opt[2]==0 and index(g,x,y-1):g[y-1][x][1][0]='-'
-                                    elif opt[2]==1 and index(g,x+1,y):g[y][x+1][1][1]='|'
-                                    elif opt[2]==2:g[y][x][1][0]='-'
-                                    elif opt[2]==3:g[y][x][1][1]='|'
-                                d=[g[y][x][0],g[opt[1]][opt[0]][0]]
-                                if d not in dom_:d.reverse()
-                                if d in dom:
-                                    g_,dom_,d_doms_=remove(g_,[[x,y],opt[:2],d],dom_,d_doms_)
-                                    p=[]
-                                    exit_=True
-                                    for d in dom_:
-                                        o=occ(g_,d)
-                                        if len(o)==1 and o[0][:2] in p:exit_=False
-                                        else:
-                                            b=False
-                                            for a in o:
-                                                if a[:2] not in p:p+=a[:2];b=True
-                                            if not b:exit_=False
-                                     if exit_:g,dom,d_doms=remove(g,[[x,y],opt[:2],d],dom,d_doms);done=False
-                                else:
-                                    if opt[2]==0 and index(g,x,y-1):g[y-1][x][1][0]='-'
-                                    elif opt[2]==1 and index(g,x+1,y):g[y][x+1][1][1]='|'
-                                    elif opt[2]==2:g[y][x][1][0]='-'
-                                    elif opt[2]==3:g[y][x][1][1]='|'"""
-        #M4
-        for d in dom:
-            if len(occ(g,d))>0:
-                o=occ(g,d)
-                a=[]
-                for p in o:a+=p[:2]
-                if a.count(a[0])==len(o):
-                    x,y=a[0][0],a[0][1]
-                    for w in wall_cnt(g,x,y)[1]:
-                        if g[w[1]][w[0]][0]!=d[1]:
-                            if w[2]==0 and index(g,x,y-1):g[y-1][x][1][0]='-'
-                            elif w[2]==1 and index(g,x+1,y):g[y][x+1][1][1]='|'
-                            elif w[2]==2:g[y][x][1][0]='-'
-                            elif w[2]==3:g[y][x][1][1]='|'
-                elif a.count(a[1])==len(o):
-                    x,y=a[1][0],a[1][1]
-                    for w in wall_cnt(g,x,y)[1]:
-                        if g[w[1]][w[0]][0]!=d[0]:
-                            if w[2]==0 and index(g,x,y-1):g[y-1][x][1][0]='-'
-                            elif w[2]==1 and index(g,x+1,y):g[y][x+1][1][1]='|'
-                            elif w[2]==2:g[y][x][1][0]='-'
-                            elif w[2]==3:g[y][x][1][1]='|'
-        #M5
-        #M6
-        for y in range(H):
-            for x in range(W):
-                if index(g,x,y+1) and (g[y][x][1][1]=='|' or x==0) and((index(g,x,y-1)and g[y-1][x][1][0]=='-') or (not index(g,x,y-1))) and ((index(g,x+1,y)and g[y][x+1][1][1]=='|') or (not index(g,x+1,y))):
-                    d=False
-                    if [g[y][x][0],g[y+1][x][0]] in dom:d=[g[y][x][0],g[y+1][x][0]]
-                    if [g[y+1][x][0],g[y][x][0]] in dom:d=[g[y+1][x][0],g[y][x][0]]
-                    if d:g,dom,d_doms=remove(g,[[x,y],[x,y+1],d],dom,d_doms);done=False
-                if index(g,x,y-1) and (g[y][x][1][1]=='|' or x==0) and (g[y][x][1][0]=='-' or y==H-1) and((index(g,x+1,y)and g[y][x+1][1][1]=='|') or (not index(g,x+1,y))):
-                    d=False
-                    if [g[y][x][0],g[y-1][x][0]] in dom:d=[g[y][x][0],g[y-1][x][0]]
-                    if [g[y-1][x][0],g[y][x][0]] in dom:d=[g[y-1][x][0],g[y][x][0]]
-                    if d:g,dom,d_doms=remove(g,[[x,y],[x,y-1],d],dom,d_doms);done=False
-                if index(g,x+1,y) and (g[y][x][1][1]=='|' or x==0) and (g[y][x][1][0]=='-' or y==H-1) and((index(g,x,y-1)and g[y-1][x][1][0]=='-') or (not index(g,x,y-1))):
-                    d=False
-                    if [g[y][x][0],g[y][x+1][0]] in dom:d=[g[y][x][0],g[y][x+1][0]]
-                    if [g[y][x+1][0],g[y][x][0]] in dom:d=[g[y][x+1][0],g[y][x][0]]
-                    if d:g,dom,d_doms=remove(g,[[x,y],[x+1,y],d],dom,d_doms);done=False
-                if index(g,x-1,y) and (g[y][x][1][0]=='-' or y==H-1) and((index(g,x,y-1)and g[y-1][x][1][0]=='-') or (not index(g,x,y-1))) and ((index(g,x+1,y)and g[y][x+1][1][1]=='|') or (not index(g,x+1,y))):
-                    d=False
-                    if [g[y][x][0],g[y][x-1][0]] in dom:d=[g[y][x][0],g[y][x-1][0]]
-                    if [g[y][x-1][0],g[y][x][0]] in dom:d=[g[y][x-1][0],g[y][x][0]]
-                    if d:g,dom,d_doms=remove(g,[[x,y],[x-1,y],d],dom,d_doms);done=False
-        if done:break
-    return [dom,g,print_format(g)]
-if __name__=='__main__':
-    #g=['304022','141432','130221','010333','214044'];d=dominos(4)
-    #g=['064150165','121663757','164222710','432665634','014023635','373252075','521743477','450041703'];d=dominos(6)
-    #g=['1370158430','5752704883','4702755234','4268873600','6106122671','6535817401','5241276853','4261880463','6514378302'];d=dominos(8)
-    g=['57420004554','33960773219','71640263397','52516875993','92634381490','38290517845','05288460846','88674627936','30111765912','97185412082'];d=dominos(9)
-    s=solve(g,d)
-    print(f'{len(s[0])}\n{s[2]}\n{s[0]}')
+def print_board(b,dm=None):
+	def set_char(s,i,c):
+		if (s[i]=="+"):return s
+		return s[:i]+c+s[i+1:]
+	w=len(b[0])
+	h=len(b)
+	s="+"+"-"*(2*w-1)+"+\n"
+	for i in range(0,h):
+		s+="|"
+		for j in range(0,w):
+			s+=b[i][j]+" "
+		s=s[:len(s)-1]+"|\n|"+"  "*w
+		s=s[:len(s)-1]+"|\n"
+	s=s[:len(s)-w*2-2]+"+"+"-"*(2*w-1)+"+"
+	if (dm==None):
+		print(s+"\n")
+	else:
+		s=s.split("\n")
+		for d in dm:
+			s[d["p1"][1]*2]=set_char(s[d["p1"][1]*2],d["p1"][0]*2,"+")
+			s[d["p1"][1]*2]=set_char(s[d["p1"][1]*2],d["p1"][0]*2+1,"-")
+			s[d["p1"][1]*2+1]=set_char(s[d["p1"][1]*2+1],d["p1"][0]*2,"|")
+			if (d["p1"][0]<d["p2"][0]):
+				s[d["p1"][1]*2+2]=set_char(s[d["p1"][1]*2+2],d["p1"][0]*2,"+")
+				s[d["p1"][1]*2]=set_char(s[d["p1"][1]*2],d["p1"][0]*2+4,"+")
+				s[d["p1"][1]*2+2]=set_char(s[d["p1"][1]*2+2],d["p1"][0]*2+4,"+")
+				s[d["p1"][1]*2]=set_char(s[d["p1"][1]*2],d["p1"][0]*2+1,"-")
+				s[d["p1"][1]*2]=set_char(s[d["p1"][1]*2],d["p1"][0]*2+2,"-")
+				s[d["p1"][1]*2]=set_char(s[d["p1"][1]*2],d["p1"][0]*2+3,"-")
+				s[d["p1"][1]*2+2]=set_char(s[d["p1"][1]*2+2],d["p1"][0]*2+1,"-")
+				s[d["p1"][1]*2+2]=set_char(s[d["p1"][1]*2+2],d["p1"][0]*2+2,"-")
+				s[d["p1"][1]*2+2]=set_char(s[d["p1"][1]*2+2],d["p1"][0]*2+3,"-")
+				s[d["p1"][1]*2+1]=set_char(s[d["p1"][1]*2+1],d["p1"][0]*2+4,"|")
+			else:
+				s[d["p1"][1]*2+4]=set_char(s[d["p1"][1]*2+4],d["p1"][0]*2,"+")
+				s[d["p1"][1]*2]=set_char(s[d["p1"][1]*2],d["p1"][0]*2+2,"+")
+				s[d["p1"][1]*2+4]=set_char(s[d["p1"][1]*2+4],d["p1"][0]*2+2,"+")
+				s[d["p1"][1]*2+1]=set_char(s[d["p1"][1]*2+1],d["p1"][0]*2,"|")
+				s[d["p1"][1]*2+2]=set_char(s[d["p1"][1]*2+2],d["p1"][0]*2,"|")
+				s[d["p1"][1]*2+3]=set_char(s[d["p1"][1]*2+3],d["p1"][0]*2,"|")
+				s[d["p1"][1]*2+1]=set_char(s[d["p1"][1]*2+1],d["p1"][0]*2+2,"|")
+				s[d["p1"][1]*2+2]=set_char(s[d["p1"][1]*2+2],d["p1"][0]*2+2,"|")
+				s[d["p1"][1]*2+3]=set_char(s[d["p1"][1]*2+3],d["p1"][0]*2+2,"|")
+				s[d["p1"][1]*2+4]=set_char(s[d["p1"][1]*2+4],d["p1"][0]*2+1,"-")
+		print("\n".join(s))
+def print_dominos(dl):
+	for  d in dl:
+		print(str(d[0])+"|"+str(d[1]))
+
+
+
+def random_board(s):
+	def set_char(s,i,c):
+		return s[:i]+c+s[i+1:]
+	b=[" "*(s+1)]*s
+	dominos=[]
+	for n in range(s):
+		for n2 in range(s):
+			if ([n2,n] in dominos):continue
+			dominos+=[[n,n2]]
+	random.shuffle(dominos)
+	x,y=0,0
+	while (len(dominos)>0):
+		while (x>s or b[y][x]!=" "):
+			x+=1
+			if (x>s):
+				x=0
+				y+=1
+		d=random.randint(0,1)
+		if (random.randint(0,1)==0):
+			dominos[0].reverse()
+		move=False
+		for e in range(0,2):
+			if (d==0):
+				if (x+1<=s and b[y][x+1]==" "):
+					b[y]=set_char(b[y],x,str(dominos[0][0]))
+					b[y]=set_char(b[y],x+1,str(dominos[0][1]))
+					dominos.remove(dominos[0])
+					move=True
+					break
+			if (d==1):
+				if (y+1<s and b[y+1][x]==" "):
+					b[y]=set_char(b[y],x,str(dominos[0][0]))
+					b[y+1]=set_char(b[y+1],x,str(dominos[0][1]))
+					dominos.remove(dominos[0])
+					move=True
+					break
+			d=1-d
+		if (move==False):
+			return random_board(s)
+	return b
+
+
+
+def solve_board(b,log=False,dt=None):
+	def copy_data(dominos,dl,done_tiles):
+		dt=[[],[],[]]
+		for d in dominos:
+			dt[0].append({"d":[d["d"][0],d["d"][1]],"p1":[d["p1"][0],d["p1"][1]],"p2":[d["p2"][0],d["p2"][1]]})
+		for d in dl:
+			dt[1].append([d[0],d[1]])
+		for t in done_tiles:
+			dt[2].append([t[0],t[1]])
+		return dt
+	def step1(b,done_tiles,w,h,dominos,dl):
+		le=[]
+		for x in range(0,w):
+			for y in range(0,h):
+				if ([x,y] in done_tiles):continue
+				if (x+1<w and [x+1,y] not in done_tiles and [min(int(b[y][x]),int(b[y][x+1])),max(int(b[y][x]),int(b[y][x+1]))] in dl):
+					le+=[[[min(int(b[y][x]),int(b[y][x+1])),max(int(b[y][x]),int(b[y][x+1]))],x,y,x+1,y]]
+				if (y+1<h and [x,y+1] not in done_tiles and [min(int(b[y][x]),int(b[y+1][x])),max(int(b[y][x]),int(b[y+1][x]))] in dl):
+					le+=[[[min(int(b[y][x]),int(b[y+1][x])),max(int(b[y][x]),int(b[y+1][x]))],x,y,x,y+1]]
+		empty=[]
+		done=[]
+		for dp in le:
+			if (dp[0] in done):
+				for k in empty:
+					if (k["d"]==dp[0]):
+						empty.remove(k)
+			else:
+				done.append(dp[0])
+				empty.append({"d":dp[0],"p1":dp[1:3],"p2":dp[3:5]})
+		for t in empty:
+			done_tiles+=[[t["p1"][0],t["p1"][1]],[t["p2"][0],t["p2"][1]]]
+			dl.remove(t["d"])
+		dominos+=empty
+		return (len(empty)>0)
+	def step2(b,done_tiles,w,h,dominos,dl):
+		l=[]
+		for x in range(0,w):
+			for y in range(0,h):
+				if ([x,y] in done_tiles):continue
+				s=0
+				d=0
+				i=0
+				for da in [[-1,0],[0,-1],[1,0],[0,1]]:
+					nx,ny=x+da[0],y+da[1]
+					if (nx<0 or nx>=w or ny<0 or ny>=h or [nx,ny] in done_tiles or [min(int(b[y][x]),int(b[ny][nx])),max(int(b[y][x]),int(b[ny][nx]))] not in dl):
+						s+=1
+					else:
+						d=i+0
+					i+=1
+				if (s==3):
+					l+=[[x,y,d]]
+		rmd=[]
+		done=[]
+		for dp in l:
+			if (dp[:2] in done):
+				for k in rmd:
+					if (rmd["p1"][0]==dp[0] and rmd["p1"][1]==dp[1]):
+						rmd.remove(k)
+			else:
+				done.append(dp[:2])
+				dr=[[-1,0],[0,-1],[1,0],[0,1]][dp[2]]
+				rmd.append({"d":[min(int(b[dp[1]][dp[0]]),int(b[dp[1]+dr[1]][dp[0]+dr[0]])),max(int(b[dp[1]][dp[0]]),int(b[dp[1]+dr[1]][dp[0]+dr[0]]))],"p1":[min(dp[0],dp[0]+dr[0]),min(dp[1],dp[1]+dr[1])],"p2":[max(dp[0],dp[0]+dr[0]),max(dp[1],dp[1]+dr[1])]})
+		for t in rmd:
+			if (t["d"] not in dl):continue
+			done_tiles+=[[t["p1"][0],t["p1"][1]],[t["p2"][0],t["p2"][1]]]
+			dl.remove(t["d"])
+			dominos.append(t)
+		return (len(rmd)>0)
+	def step3(b,done_tiles,w,h,dominos,dl):
+		for x in range(0,w):
+			for y in range(0,h):
+				if ([x,y] in done_tiles):continue
+				s=0
+				d=[]
+				i=0
+				for da in [[-1,0],[0,-1],[1,0],[0,1]]:
+					nx,ny=x+da[0],y+da[1]
+					if (nx<0 or nx>=w or ny<0 or ny>=h or [nx,ny] in done_tiles or [min(int(b[y][x]),int(b[ny][nx])),max(int(b[y][x]),int(b[ny][nx]))] not in dl):
+						s+=1
+					else:
+						d+=[i]
+					i+=1
+				if (s==2):
+					for dr in d:
+						dr=[[-1,0],[0,-1],[1,0],[0,1]][dr]
+						dt=copy_data(dominos,dl,done_tiles)
+						m={"d":[int(min(b[y][x],b[y+dr[1]][x+dr[0]])),int(max(b[y][x],b[y+dr[1]][x+dr[0]]))],"p1":[min(x,x+dr[0]),min(y,y+dr[1])],"p2":[max(x,x+dr[0]),max(y,y+dr[1])]}
+						dt[0].append(m)
+						dt[1].remove(m["d"])
+						dt[2]+=[[m["p1"][0],m["p1"][1]],[m["p2"][0],m["p2"][1]]]
+						s,_,_=solve_board(b,dt=dt)
+						if (s==True):
+							dominos.append(m)
+							dl.remove(m["d"])
+							done_tiles+=[[m["p1"][0],m["p1"][1]],[m["p2"][0],m["p2"][1]]]
+							return True
+					return False
+		return False
+	w=len(b[0])
+	h=len(b)
+	dominos=[]
+	dl=[]
+	dn=int(max("".join(b).replace("","$").split("$")))+1
+	for n in range(dn):
+		for n2 in range(dn):
+			if ([n2,n] in dl):continue
+			dl+=[[n,n2]]
+	done_tiles=[]
+	if (dt!=None):
+		dominos,dl,done_tiles=dt[0],dt[1],dt[2]
+	while True:
+		if (log==True):
+			print_board(b,dominos)
+			print("\n")
+		s=step1(b,done_tiles,w,h,dominos,dl)
+		if (s==False):
+			s=step2(b,done_tiles,w,h,dominos,dl)
+			if (s==False):
+				s=step3(b,done_tiles,w,h,dominos,dl)
+		if (len(dl)==0):
+			return (True,dominos,dl);
+		if (s==False):
+			return (False,dominos,dl)
+
+
+
+def test():
+	score=0
+	for i in range(5):
+		b=random_board(random.randint(2,9))
+		s,d,dl=solve_board(b,log=False)
+		if (s==False):
+			print("Fail:(")
+			print_dominos(dl)
+		else:
+			print("Success!")
+			print_board(b,d)
+			score+=1
+	print("\n"+"="*30+"\n"+f"Score: {score}/{i+1}".center(30," ")+"\n"+"="*30)
+
+
+
+if (__name__=="__main__"):
+	test()
